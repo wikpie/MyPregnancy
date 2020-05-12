2069c7e20c491d0e6cd6166dc9734b558c6c243a
wikpie <wiktor.pienko@gmail.com>
2020-05-12 13:29:28 +0200

    Cleanup

--- app/src/main/java/wiktor/pienko/mypregnancy/Constants.kt
+++ app/src/main/java/wiktor/pienko/mypregnancy/Constants.kt
@@ -1,11 +1,4 @@
 package wiktor.pienko.mypregnancy
 
-// add constants here
 
-//base url for retrofit connection
-const val BASE_URL: String = ""
-//api for connection
-const val API:String=""
-const val ACTIVITY_REQUEST_CODE = 1
-const val EXTRA_REPLY = ""
 
--- app/src/main/java/wiktor/pienko/mypregnancy/fragments/diet/DietFragment.kt
+++ app/src/main/java/wiktor/pienko/mypregnancy/fragments/diet/DietFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_diet.view.*
 import wiktor.pienko.mypregnancy.R
 
 class DietFragment : Fragment(){
